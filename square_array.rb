cdaf8691c417351221d2b9f6e373de64e4ba3c03
Laura Berge <lauraeberge@gmail.com>
2019-09-09 15:46:07 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,20 @@
 def square_array(array)
-  # your code here
+  squared = []
+  array.each do |element|
+    element *= element
+    squared.push(element)
+  end
+  squared
 end
+
+def square_array_with_collect(array)
+  p array.collect {|element| element * element}
+end
+
+numbers = [1,2,3]
+
+square_array_with_collect(numbers)
+
+new_numbers = [9,10,16,25]
+
+square_array_with_collect(new_numbers)
