9d3b213e16479a739c89c60963be6adc7e3a1943
Anthony N Boyd <anboyd97@gmail.com>
2019-09-22 02:48:19 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  new_array=[]
+  counter=0
+  array.each do |number|
+    new_array[counter]=number*number
+    counter+=1
+  end
+  return new_array
 end
