00b5c2ac82f0ad254792ec139396c45c32f2d2af
Ally Morson <ally.morson@gmail.com>
2019-09-27 15:33:31 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
- .each 
+  squared = []
+  #calling each on array
+  #iterate through array
+  #square each element and add to squared array 
+  array.each do |element| squared << element ** 2 
+end
+squared
 end
